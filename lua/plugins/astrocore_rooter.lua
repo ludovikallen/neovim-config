7824de6f10e2b823d486d99b470b8c68fb7af027
Ludovik Allen <ludovikallen@gmail.com>
2025-03-06 15:03:09 -0500

    Activated automatic rooter

---
+++ lua/plugins/astrocore_rooter.lua
@@ -0,0 +1,14 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure project root detection, check status with `:AstroRootInfo`
+    rooter = {
+      autochdir = true,
+      -- scope of working directory to change ("global"|"tab"|"win")
+      scope = "global",
+      -- show notification on every working directory change
+      notify = true,
+    },
+  },
+}
